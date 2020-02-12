afcc248934e330a1b4350b02ef6165578a7d35f4
Shake&Bake <jakestengercabrera@gmail.com>
2020-02-11 16:43:07 -0800

    autoCommit Tue 02/11/2020 16:43

--- auto_commit.bat
+++ auto_commit.bat
@@ -1,9 +1,5 @@
 cd /D "%~dp0"
-whoami >> test123.txt
 git add --all
-echo added >> test123.txt
 git commit -m "autoCommit %date% %time:~0,5%"
-echo committed >> test123.txt
 git push
-echo pushed >> test123.txt
 exit
